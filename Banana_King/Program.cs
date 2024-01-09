b9fd133beb18ff2b01c2fd4ee5f4700e99ddc966
aminsys <amin.yassin@live.com>
2024-01-09 11:20:50 +0100

    Added default identity management tool

--- Banana_King/Program.cs
+++ Banana_King/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Banana_King.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FruitEmpireAuthConnection") ?? throw new InvalidOperationException("Connection string 'FruitEmpireAuthConnection' not found.");
+
+builder.Services.AddDbContext<FruitEmpireAuth>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FruitEmpireAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
